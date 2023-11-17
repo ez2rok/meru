b0276990e27b13c3eb1e991b9de96646c0d14788
eitan <eitan.turok@gmail.com>
2023-11-17 08:46:24 -0500

    configs with diff combos of encoder, proj, and norm

---
+++ configs/evals/linprobe_classification_EPX.py
@@ -0,0 +1,20 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from meru.config import LazyCall as L
+from meru.evaluation.classification import LinearProbeClassificationEvaluator
+
+
+evaluator = L(LinearProbeClassificationEvaluator)(
+    datasets=[
+        "food101",
+    ],
+    data_dir="datasets/eval",
+    image_size=224,
+    num_workers=2,
+    proj=True,
+    norm=False,
+)
