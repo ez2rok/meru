7c366807e92506f4dd153e61d14c4e5c421a3129
eitan <eitan.turok@gmail.com>
2023-11-19 12:03:21 -0500

    zero-shot clf must have proj layer b/c text encoder and visual proj have diff dims

--- scripts/embed.sh
+++ scripts/embed.sh
@@ -34,13 +34,13 @@ python scripts/evaluate.py \
    
 ############ Zero-shot Classification ############
 
-# Evaluate embeddings with no proj and no norm.
-python scripts/evaluate.py \
-   --config configs/embeddings/zero_shot_classification_EXX.py \
-   --checkpoint-path checkpoints/meru_vit_s.pth \
-   --train-config output/meru_vit_small_0512/config.yaml \
-   --save-eval-artifacts \
-   --save-eval-results
+# # Evaluate embeddings with no proj and no norm.
+# python scripts/evaluate.py \
+#    --config configs/embeddings/zero_shot_classification_EXX.py \
+#    --checkpoint-path checkpoints/meru_vit_s.pth \
+#    --train-config output/meru_vit_small_0512/config.yaml \
+#    --save-eval-artifacts \
+#    --save-eval-results
 
 # Evaluate embeddings with proj and no norm.
 python scripts/evaluate.py \
@@ -50,13 +50,13 @@ python scripts/evaluate.py \
    --save-eval-artifacts \
    --save-eval-results
    
-# Evaluate embeddings with no proj and norm.
-python scripts/evaluate.py \
-   --config configs/embeddings/zero_shot_classification_EXN.py \
-   --checkpoint-path checkpoints/meru_vit_s.pth \
-   --train-config output/meru_vit_small_0512/config.yaml \
-   --save-eval-artifacts \
-   --save-eval-results
+# # Evaluate embeddings with no proj and norm.
+# python scripts/evaluate.py \
+#    --config configs/embeddings/zero_shot_classification_EXN.py \
+#    --checkpoint-path checkpoints/meru_vit_s.pth \
+#    --train-config output/meru_vit_small_0512/config.yaml \
+#    --save-eval-artifacts \
+#    --save-eval-results
 
 # Evaluate embeddings with proj and norm.
 python scripts/evaluate.py \
