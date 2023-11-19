1a27537ea44c84d05bc43bc8ba5394fcc19314e0
eitan <eitan.turok@gmail.com>
2023-11-19 09:25:00 -0500

    compuyte linprobe embeddings before zero-shot classifcation embeddings

--- scripts/embed.sh
+++ scripts/embed.sh
@@ -1,3 +1,37 @@
+############ Linear Probe ############
+
+# Evaluate embeddings with no proj and no norm.
+python scripts/evaluate.py \
+   --config configs/embeddings/linprobe_classification_EXX.py \
+   --checkpoint-path checkpoints/meru_vit_s.pth \
+   --train-config output/meru_vit_small_0512/config.yaml \
+   --save-eval-artifacts \
+   --save-eval-results
+
+# Evaluate embeddings with proj and no norm.
+python scripts/evaluate.py \
+   --config configs/embeddings/linprobe_classification_EPX.py \
+   --checkpoint-path checkpoints/meru_vit_s.pth \
+   --train-config output/meru_vit_small_0512/config.yaml \
+   --save-eval-artifacts \
+   --save-eval-results
+
+# Evaluate embeddings with no proj and norm.
+python scripts/evaluate.py \
+   --config configs/embeddings/linprobe_classification_EXN.py \
+   --checkpoint-path checkpoints/meru_vit_s.pth \
+   --train-config output/meru_vit_small_0512/config.yaml \
+   --save-eval-artifacts \
+   --save-eval-results
+   
+# Evaluate embeddings with proj and norm.
+python scripts/evaluate.py \
+   --config configs/embeddings/linprobe_classification_EPN.py \
+   --checkpoint-path checkpoints/meru_vit_s.pth \
+   --train-config output/meru_vit_small_0512/config.yaml \
+   --save-eval-artifacts \
+   --save-eval-results
+   
 ############ Zero-shot Classification ############
 
 # Evaluate embeddings with no proj and no norm.
@@ -61,40 +95,6 @@ python scripts/evaluate.py \
 # Evaluate embeddings with proj and norm.
 python scripts/evaluate.py \
    --config configs/embeddings/zero_shot_retrieval_EPN.py \
-   --checkpoint-path checkpoints/meru_vit_s.pth \
-   --train-config output/meru_vit_small_0512/config.yaml \
-   --save-eval-artifacts \
-   --save-eval-results
-
-############ Linear Probe ############
-
-# Evaluate embeddings with no proj and no norm.
-python scripts/evaluate.py \
-   --config configs/embeddings/linprobe_classification_EXX.py \
-   --checkpoint-path checkpoints/meru_vit_s.pth \
-   --train-config output/meru_vit_small_0512/config.yaml \
-   --save-eval-artifacts \
-   --save-eval-results
-
-# Evaluate embeddings with proj and no norm.
-python scripts/evaluate.py \
-   --config configs/embeddings/linprobe_classification_EPX.py \
-   --checkpoint-path checkpoints/meru_vit_s.pth \
-   --train-config output/meru_vit_small_0512/config.yaml \
-   --save-eval-artifacts \
-   --save-eval-results
-
-# Evaluate embeddings with no proj and norm.
-python scripts/evaluate.py \
-   --config configs/embeddings/linprobe_classification_EXN.py \
-   --checkpoint-path checkpoints/meru_vit_s.pth \
-   --train-config output/meru_vit_small_0512/config.yaml \
-   --save-eval-artifacts \
-   --save-eval-results
-   
-# Evaluate embeddings with proj and norm.
-python scripts/evaluate.py \
-   --config configs/embeddings/linprobe_classification_EPN.py \
    --checkpoint-path checkpoints/meru_vit_s.pth \
    --train-config output/meru_vit_small_0512/config.yaml \
    --save-eval-artifacts \
